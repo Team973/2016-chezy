ac66fb5e67b146c0c28cd50abafb5860de053ed2
Andrew Nelson <andrew2nelson@gmail.com>
2016-08-12 23:44:16 -0500

    no magic number 14 convert from degree to click

--- robot-thing/src/subsystems/Turret.cpp
+++ robot-thing/src/subsystems/Turret.cpp
@@ -12,6 +12,8 @@
 
 namespace frc973 {
 
+constexpr double CLICKS_PER_DEGREE = 14.0;
+
 Turret::Turret(TaskMgr *scheduler)
 		: m_turretMotor(new CANTalon(SHOOTER_TURRET_CAN_ID))
 		, m_autoTargetEnabled(false)
@@ -22,8 +24,8 @@ Turret::Turret(TaskMgr *scheduler)
 	m_scheduler->RegisterTask("Shooter", this, TASK_PERIODIC);
 	m_turretMotor->SetFeedbackDevice(CANTalon::FeedbackDevice::QuadEncoder);
 	m_turretMotor->SetSensorDirection(true);
-	m_turretMotor->ConfigForwardLimit(80.0 * 14.0);
-	m_turretMotor->ConfigReverseLimit(-80.0 * 14.0);
+	m_turretMotor->ConfigForwardLimit(80.0 * CLICKS_PER_DEGREE);
+	m_turretMotor->ConfigReverseLimit(-80.0 * CLICKS_PER_DEGREE);
 }
 
 Turret::~Turret() {
@@ -32,7 +34,7 @@ Turret::~Turret() {
 
 void Turret::SetTurretPosition(double pos) {
 	m_turretMotor->SetControlMode(CANTalon::ControlMode::kPosition);
-	m_turretMotor->Set(pos);
+	m_turretMotor->Set(pos * CLICKS_PER_DEGREE);
 }
 
 double Turret::GetTurretPosition() {
